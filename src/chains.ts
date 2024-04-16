aa0d8aabe0ebb473620a4e5e69def8787a9fb478
workwithhim <dev753951789@gmail.com>
2024-04-15 17:38:59 -0700

    remove other chains except mainnet, sepolia and coston2

--- src/chains.ts
+++ src/chains.ts
@@ -1,32 +1,12 @@
 export enum ChainId {
   MAINNET = 1,
-  GOERLI = 5,
   SEPOLIA = 11155111,
-  OPTIMISM = 10,
-  OPTIMISM_GOERLI = 420,
-  OPTIMISM_SEPOLIA = 11155420,
-  ARBITRUM_ONE = 42161,
-  ARBITRUM_GOERLI = 421613,
-  ARBITRUM_SEPOLIA = 421614,
-  POLYGON = 137,
-  POLYGON_MUMBAI = 80001,
-  CELO = 42220,
-  CELO_ALFAJORES = 44787,
-  GNOSIS = 100,
-  MOONBEAM = 1284,
-  BNB = 56,
-  AVALANCHE = 43114,
-  BASE_GOERLI = 84531,
-  BASE = 8453,
-  ZORA = 7777777,
-  ZORA_SEPOLIA = 999999999,
-  ROOTSTOCK = 30,
-  BLAST = 81457,
   COSTON2 = 114
 }
 
 export const SUPPORTED_CHAINS = [
   ChainId.MAINNET,
+  ChainId.SEPOLIA,
   ChainId.COSTON2
 ] as const
 export type SupportedChainsType = typeof SUPPORTED_CHAINS[number]
@@ -34,12 +14,5 @@ export type SupportedChainsType = typeof SUPPORTED_CHAINS[number]
 export enum NativeCurrencyName {
   // Strings match input for CLI
   ETHER = 'ETH',
-  MATIC = 'MATIC',
-  CELO = 'CELO',
-  GNOSIS = 'XDAI',
-  MOONBEAM = 'GLMR',
-  BNB = 'BNB',
-  AVAX = 'AVAX',
-  ROOTSTOCK = 'RBTC',
   COSTON2 = 'C2FLR',
 }
