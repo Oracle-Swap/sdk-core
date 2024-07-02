814efe2274492d53edf0bef5b6285e9589ea10e9
workwithhim <dev753951789@gmail.com>
2024-07-02 16:19:06 -0300

    fix console

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,4 +1,4 @@
-import { ChainId, SUPPORTED_CHAINS, SupportedChainsType } from './chains'
+import { ChainId, SUPPORTED_CHAINS } from './chains'
 
 export type AddressMap = { [chainId: number]: string }
 
